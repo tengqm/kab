88ecaf1b033ab27df15ad8fcd36839e7fc3e96ef
Qiming Teng <tengqm@outlook.com>
2025-09-25 21:05:16 +0800

    Update build script

--- kab/build.sh
+++ kab/build.sh
@@ -37,7 +37,9 @@ function build_wheel() {
             rm -rf $d
         fi
     done
-    sudo pip3 install --upgrade setuptools wheel
+    python3 -m venv venv
+    source venv/bin/activate
+    python3 -m pip install --upgrade setuptools wheel
     python3 setup.py bdist_wheel
 
     popd
