47fe1461c0e7556481d6c00de3d8d14efb8483d1
Qiming Teng <tengqm@outlook.com>
2025-09-25 21:09:37 +0800

    Update build requirements

--- kab/build.sh
+++ kab/build.sh
@@ -39,6 +39,7 @@ function build_wheel() {
     done
     python3 -m venv venv
     source venv/bin/activate
+    python3 -m pip install --upgrade pip
     python3 -m pip install --upgrade setuptools wheel
     python3 setup.py bdist_wheel
 
